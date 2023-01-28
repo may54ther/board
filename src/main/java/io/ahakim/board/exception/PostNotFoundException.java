849889c2c1c0134819edbe48710f61eba8d7d872
may54ther <aha9654@gmail.com>
2023-01-29 04:38:11 +0900

    refactor: CustomException 제거

--- src/main/java/io/ahakim/board/exception/PostNotFoundException.java
+++ src/main/java/io/ahakim/board/exception/PostNotFoundException.java
@@ -1,6 +1,6 @@
 package io.ahakim.board.exception;
 
-public class PostNotFoundException extends CustomException {
+public class PostNotFoundException extends RuntimeException {
 
     public PostNotFoundException() {
         super("게시글이 존재하지 않습니다.");
