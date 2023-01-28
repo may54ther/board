cc12549243afd1e1d33ccb76398c9d0a14e8d535
may54ther <aha9654@gmail.com>
2023-01-29 02:18:49 +0900

    feat: 입력 폼 검증, 이벤트 관련 js

--- src/main/resources/static/js/post.js
+++ src/main/resources/static/js/post.js
@@ -26,3 +26,42 @@ const Editor = EL_EDITOR ?
             }
         }
     }) : null;
+
+/////////////////////////////////////////////////
+// functions
+/////////////////////////////////////////////////
+var postFormAction = {
+    getList: function () {
+        location.href = `/posts`;
+    },
+    getDetail: function (postId) {
+        location.href = `/posts/${postId}`;
+    },
+    addForm: function () {
+        location.href = `/posts/add`;
+    },
+    replyForm: function (postId) {
+        location.href = `/posts/${postId}/reply`;
+    },
+    editForm: function (postId) {
+        location.href = `/posts/${postId}/edit`;
+    },
+    removePost: function (postId) {
+        if (confirm("게시글을 삭제하시겠습니까?")) {
+            location.href = `/posts/${postId}/remove`;
+        }
+    },
+    savePost: function (e) {
+        e.preventDefault();
+
+        if (isNullOrEmpty(writer.value)) {
+            showErrorToast("작성자를 입력하세요.");
+        } else if (isNullOrEmpty(title.value)) {
+            showErrorToast("제목을 입력하세요.")
+        } else if (isNullOrEmpty(content.value)) {
+            showErrorToast("내용을 입력하세요.");
+        } else {
+            postForm.submit();
+        }
+    },
+}
